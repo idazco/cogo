4b1f41ca0e0942c2b56ca47495afe6fe81f86f9f
sylnsr <git@idaz.co>
2021-07-09 13:16:18 -0500

    Add MAcAddresses function

--- environment/environment.go
+++ environment/environment.go
@@ -1,6 +1,7 @@
 package environment
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net"
@@ -8,6 +9,7 @@ import (
 	"path/filepath"
 )
 
+// Hostname returns the host name recognized by the operating system
 func Hostname() string {
 	hostNameStr, err := os.Hostname()
 	if err != nil {
@@ -17,7 +19,7 @@ func Hostname() string {
 	}
 }
 
-// Get preferred outbound ip of this machine
+// OutboundIP gets the preferred outbound ip of the machine
 func OutboundIP() net.IP {
 	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
@@ -30,10 +32,25 @@ func OutboundIP() net.IP {
 	return localAddr.IP
 }
 
+// AppPath returns the absolute path to the running golang app
 func AppPath() (string, error) {
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
 		return "", err
 	}
 	return fmt.Sprint(dir), nil
+}
+
+// MacAddresses returns all the mac addresses as a map, with the name of the interface as the key
+func MacAddresses() (result map[string]string) {
+	interfaces, err := net.Interfaces()
+	result = make(map[string]string)
+	if err == nil {
+		for _, i := range interfaces {
+			if i.Flags&net.FlagUp != 0 && bytes.Compare(i.HardwareAddr, nil) != 0 {
+				result[i.Name] = i.HardwareAddr.String()
+			}
+		}
+	}
+	return
 }
